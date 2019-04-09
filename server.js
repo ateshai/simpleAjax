710617596708c4166e323173ac012a311a692ee4
Aykut Ateş <ateshai@gmail.com>
2019-04-09 10:50:43 +0300

    static files serving

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const port = 3000
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {
         root: path.join(__dirname, './')
